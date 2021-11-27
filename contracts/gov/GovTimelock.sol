db5b436ef0943c156e43e682f3f8927e42751c50
a17 <m@a17.name>
2021-11-27 16:52:28 +0300

    #1 gov timelock controller

---
+++ contracts/gov/GovTimelock.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/governance/TimelockController.sol";
+
+contract GovTimelock is TimelockController {
+    constructor(
+        uint256 minDelay,
+        address[] memory proposers,
+        address[] memory executors
+    ) TimelockController(minDelay, proposers, executors) {}
+}
