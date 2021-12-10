e47eebbe96ee46eaf946c0005285d27d1a3aa4cd
a17 <m@a17.name>
2021-12-10 10:31:00 +0300

    #4 DividendPayer

---
+++ contracts/payer/DividendPayer.sol
@@ -0,0 +1,256 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20SnapshotUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "../token/DividendToken.sol";
+
+/**
+ * @title DividendPayer
+ * @dev This contract allows to split ERC20 payments amoung DividendToken
+ * holders.
+ * The split is done proportional to the percentage holdings of DividendToken.
+ * Holding shares extraction is done through {ERC20Snapshot} mechanism.
+ * DividendPayer follows a _pull payment_ model. This means that
+ * payments are not automatically forwarded to the accounts but kept in this
+ * contract, and the actual transfer is triggered as a separate step by calling
+ * the {releasePayment} function.
+ *
+ * Inspired by OpenZeppelin PaymentSplitter and SharjeelSafdar ERC20PaymentSplitter
+ */
+abstract contract DividendPayer is Initializable, UUPSUpgradeable, ReentrancyGuardUpgradeable, OwnableUpgradeable {
+    struct Received {
+        uint256 snapshotId;
+        uint256 amount;
+        address from;
+    }
+    struct Payment {
+        uint256 snapshotId;
+        uint256 amount;
+        address to;
+    }
+
+    Received[] private _received;
+    mapping(address => uint256) private _totalReceivedFrom;
+    uint256 private _totalReceived;
+
+    mapping(address => Payment[]) private _payments;
+    mapping(address => uint256) private _totalPaidTo;
+
+    uint256 private _totalPaid;
+
+    DividendToken public _sharesToken;
+    ERC20Upgradeable public _paymentToken;
+
+    /**
+     * @dev Emitted when a payment is received.
+     * @param from Address from which payment is received.
+     * @param amount Amount of `_paymentToken` received.
+     */
+    event PaymentReceived(address from, uint256 amount);
+
+    /**
+     * @dev Emitted when a payment is released.
+     * @param to Address to which payment is released.
+     * @param amount Amount of `_paymentToken` released.
+     */
+    event PaymentReleased(address to, uint256 amount);
+
+    /**
+     * @dev Set the values for {_sharesToken} and {_paymentToken}.
+     * @param sharesToken_ Address of the DividendToken to use.
+     * @param paymentToken_ Address of the {ERC20} token to use.
+     */
+    function __DividendPayer_init(
+        // solhint-disable-previous-line func-name-mixedcase
+        DividendToken sharesToken_,
+        ERC20Upgradeable paymentToken_
+    ) internal initializer {
+        _sharesToken = sharesToken_;
+        _paymentToken = paymentToken_;
+        __UUPSUpgradeable_init();
+        __Ownable_init();
+    }
+
+    /**
+     * @dev Get the total amount of `_paymentToken` released so far.
+     */
+    function totalPaid() public view returns (uint256) {
+        return _totalPaid;
+    }
+
+    /**
+     * @dev Get the total amount of `_paymentToken` released to `payee` so far.
+     * @param payee Address for which total paid amount is desired.
+     */
+    function totalPaidTo(address payee) public view returns (uint256) {
+        return _totalPaidTo[payee];
+    }
+
+    /**
+     * @dev Get the total number of payments released to `payee` so far.
+     */
+    function releasedPaymentsCount(address payee) public view returns (uint256) {
+        return _payments[payee].length;
+    }
+
+    /**
+     * @dev Get the `pos`-th released payment to `payee`.
+     * @param payee Address for which payment data is desired.
+     * @param pos Index of the released payment.
+     *
+     * Requirements:
+     *
+     * - `pos` must be a valid index.
+     */
+    function releasedPaymentsData(address payee, uint256 pos) public view returns (Payment memory) {
+        require(pos < releasedPaymentsCount(payee), "Payer: out of bounds index.");
+        return _payments[payee][pos];
+    }
+
+    /**
+     * @dev Get the total amount of `_paymentToken` received so far.
+     */
+    function totalReceived() public view returns (uint256) {
+        return _totalReceived;
+    }
+
+    /**
+     * @dev Get the total amount of `_paymentToken` received from `payer` so far.
+     * @param payer Address for which total received amount is desired.
+     */
+    function totalReceivedFrom(address payer) public view returns (uint256) {
+        return _totalReceivedFrom[payer];
+    }
+
+    /**
+     * @dev Returns the number of payments received so far.
+     */
+    function receivedPaymentsCount() public view returns (uint256) {
+        return _received.length;
+    }
+
+    /**
+     * @dev Get the `pos`-th received payment.
+     * @param pos Index of the received payment.
+     *
+     * Requirements:
+     *
+     * - `pos` must be a valid index.
+     */
+    function receivedPaymentsData(uint256 pos) public view returns (Received memory) {
+        require(pos < receivedPaymentsCount(), "Payer: out of bounds index.");
+        return _received[pos];
+    }
+
+    /**
+     * @dev Get the pending payment for `payee`.
+     * @param payee Address for which pending payment is desired.
+     */
+    function paymentPending(address payee) public view returns(uint256 currentPayment) {
+        Payment[] storage payments = _payments[payee];
+        uint256 lastPaymentSnapshot = payments.length == 0 ? 0 : payments[payments.length - 1].snapshotId;
+
+        for (uint256 i = _received.length; i > 0; --i) {
+            uint256 receiveSnapshot = _received[i - 1].snapshotId;
+            if (lastPaymentSnapshot > receiveSnapshot) {
+                break;
+            }
+            uint256 sharesInReceiveSnapshot = _sharesToken.balanceOfAt(payee, receiveSnapshot);
+            uint256 totalSharesInReceiveSnapshot = _sharesToken.totalSupplyAt(receiveSnapshot);
+            currentPayment += (_received[i - 1].amount * sharesInReceiveSnapshot) / totalSharesInReceiveSnapshot;
+        }
+    }
+
+    /**
+     * @dev Receives `_paymentToken` from `sender`.
+     * @param sender Address of the sender.
+     * @param amount Amount of tokens to receive.
+     *
+     * Emits a {PaymentReceived} event.
+     * Emits a {Snapshot} event on {ERC20Shares} contract.
+     *
+     * Requirements:
+     *
+     * - Total shares must be non-zero at the time of receiving payment. Else,
+     *   payment will get locked in this contract forever with no one to claim
+     *   it.
+     * - `amount` must be non-zero.
+     * - Sender must have already allowed {ERC20PaymentSplitter} to draw
+     *   `amount` tokens from `sender` address.
+     * - This contract must have {SNAPSHOT_CREATOR} role in {ERC20Shares}.
+     */
+    function receivePayment(address sender, uint256 amount) external {
+        require(_sharesToken.totalSupply() > 0, "Payer: no share holder");
+        require(amount > 0, "Payer: receiving zero tokens.");
+        _paymentToken.transferFrom(sender, address(this), amount);
+        emit PaymentReceived(sender, amount);
+
+        _totalReceived += amount;
+        _totalReceivedFrom[sender] += amount;
+        uint256 nextSnapshotId = _sharesToken.snapshot();
+        _received.push(Received({
+        snapshotId: nextSnapshotId,
+        amount: amount,
+        from: sender
+        }));
+    }
+
+    /**
+     * @dev Releases payment (if any) to the sender of the call.
+     *
+     * Emits a {PaymentReleased} event.
+     * Emits a {Snapshot} event on {ERC20Shares} contract.
+     *
+     * Requirements:
+     *
+     * - Sender must have non-zero pending payment.
+     * - This contract must have {SNAPSHOT_CREATOR} role in {ERC20Shares}.
+     */
+    function releasePayment() public virtual nonReentrant {
+        address payee = _msgSender();
+        uint256 payment = paymentPending(payee);
+        require(payment > 0, "Account is not due any payment");
+
+        _totalPaid += payment;
+        _totalPaidTo[payee] += payment;
+        // not good that many snapshots will be
+        // uint256 nextSnapshotId = _sharesToken.snapshot();
+        uint256 currentSnapshotId = _sharesToken.getCurrentSnapshotId();
+        _payments[payee].push(Payment({
+        snapshotId: currentSnapshotId,
+        amount: payment,
+        to: payee
+        }));
+
+        _paymentToken.transfer(payee, payment);
+        emit PaymentReleased(payee, payment);
+    }
+
+    /**
+     * @dev Sends {ERC20} tokens other than the `_paymentToken` to a given address.
+     * @param token Address of the ERC20 token.
+     * @param to Address to which the tokens are to be transferred.
+     * @param amount Amount of tokens to transfer.
+     *
+     * Requirements:
+     *
+     * - Only the owner of the contract can call this.
+     * - `token` must not be the same as `_paymentToken`.
+     */
+    function drainTokens(address token, address to, uint256 amount) external onlyOwner {
+        require(token != address(_paymentToken), "Payer: can't drain paymentToken.");
+
+        IERC20Upgradeable(token).transfer(to, amount);
+    }
+
+    function _authorizeUpgrade(address newImplementation) internal onlyOwner override {
+        // solhint-disable-previous-line no-empty-blocks
+    }
+}
---
+++ deploy/EtherPayer.js
@@ -0,0 +1,139 @@
+const { ethers, upgrades } = require('hardhat')
+
+module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
+  // type proxy address for upgrade contract
+  // deployer must have upgrade access
+  const upgradeProxy = null // ropsten: '0x6BaF629618551Cb7454013F67f5d4A9119A61627'
+
+  const { save, get, deploy } = deployments
+  const { deployer, devFund } = await getNamedAccounts()
+  const chainId = await getChainId()
+
+  console.log('')
+
+  // noinspection PointlessBooleanExpressionJS
+  if (!upgradeProxy) {
+    console.log(`== EtherPayer deployment to ${hre.network.name} ==`)
+    try {
+      const deplpoyment = await get('EtherPayer')
+      console.log(
+        `EtherPayer already deployed to ${hre.network.name} at ${deplpoyment.address}`
+      )
+      return
+    } catch (e) {
+      // not deployed yet
+    }
+  } else {
+    console.log(`==== EtherPayer upgrade at ${hre.network.name} ====`)
+    console.log(`Proxy address: ${upgradeProxy}`)
+  }
+
+  const dToken = await deployments.get('DividendToken')
+
+  console.log('ChainId:', chainId)
+  console.log('Deployer address:', deployer)
+  console.log('Development Fund address:', devFund)
+  console.log('DividendToken address:', dToken.address)
+
+  // noinspection PointlessBooleanExpressionJS
+  if (!upgradeProxy) {
+    const EtherPayer = await ethers.getContractFactory('EtherPayer')
+
+    let wethAddr
+    if (chainId == 1337) {
+      const weth = await deploy('WETH9', {
+        from: deployer,
+        log: true,
+      })
+      wethAddr = weth.address
+    } else if (hre.network.name == 'mainnet') {
+      // https://etherscan.io/token/0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2
+      wethAddr = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
+    } else if (hre.network.name == 'ropsten') {
+      // https://ropsten.etherscan.io/token/0xc778417e063141139fce010982780140aa0cd5ab
+      wethAddr = '0xc778417E063141139Fce010982780140Aa0cD5Ab'
+    } else if (hre.network.name == 'rinkeby') {
+      // https://rinkeby.etherscan.io/token/0xc778417e063141139fce010982780140aa0cd5ab
+      wethAddr = '0xc778417E063141139Fce010982780140Aa0cD5Ab'
+    } else if (hre.network.name == 'goerli') {
+      // https://goerli.etherscan.io/token/0xb4fbf271143f4fbf7b91a5ded31805e42b2208d6
+      wethAddr = '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6'
+    } else if (hre.network.name == 'kovan') {
+      // https://kovan.etherscan.io/token/0xd0a1e359811322d97991e03f863a0c30c2cf029c
+      wethAddr = '0xd0A1E359811322d97991E03f863a0C30C2cF029C'
+    } else {
+      throw new Error('Unsupported network')
+    }
+
+    const etherPayer = await upgrades.deployProxy(
+      EtherPayer,
+      [dToken.address, wethAddr],
+      {
+        kind: 'uups',
+      }
+    )
+
+    await etherPayer.deployed()
+
+    const artifact = await hre.artifacts.readArtifact('EtherPayer')
+
+    await save('EtherPayer', {
+      address: etherPayer.address,
+      abi: artifact.abi,
+    })
+
+    let receipt = await etherPayer.deployTransaction.wait()
+    console.log(
+      `EtherPayer proxy deployed at: ${etherPayer.address} (block: ${
+        receipt.blockNumber
+      }) with ${receipt.gasUsed.toNumber()} gas`
+    )
+
+    // grant snapshot role to payer from dtoken
+    const SNAPSHOT_ROLE = ethers.utils.id('SNAPSHOT_ROLE')
+
+    const dTokenContract = await ethers.getContractAt(
+      'DividendToken',
+      dToken.address
+    )
+
+    let tx = await dTokenContract.grantRole(SNAPSHOT_ROLE, etherPayer.address)
+    process.stdout.write(
+      `Grant DividendToken SNAPSHOT_ROLE to EthPayer (tx: ${tx.hash})...: `
+    )
+
+    receipt = await tx.wait()
+    if (receipt.status) {
+      console.log(
+        `done (block: ${
+          receipt.blockNumber
+        }) with ${receipt.gasUsed.toNumber()} gas`
+      )
+    } else {
+      console.log(`REVERTED!`)
+    }
+  } else {
+    // try to upgrade
+    const EtherPayer = await ethers.getContractFactory('EtherPayer')
+    const etherPayer = await upgrades.upgradeProxy(upgradeProxy, EtherPayer)
+
+    const artifact = await hre.artifacts.readArtifact('EtherPayer')
+
+    await save('EtherPayer', {
+      address: etherPayer.address,
+      abi: artifact.abi,
+    })
+
+    let receipt = await etherPayer.deployTransaction.wait()
+    console.log(
+      `EtherPayer upgraded through proxy: ${etherPayer.address} (block: ${
+        receipt.blockNumber
+      }) with ${receipt.gasUsed.toNumber()} gas`
+    )
+
+    // hardhat verify --network r.. 0x
+  }
+}
+
+module.exports.tags = ['EtherPayer']
+module.exports.dependencies = ['DividendToken']
