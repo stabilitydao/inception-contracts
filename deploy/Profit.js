0b5fdfdd9e6eb3a28ac13eb41a45633ff0a90137
unknown <judeabara@gmail.com>
2022-03-26 06:16:43 +0100

    deployment scripts

---
+++ deploy/Profit.js
@@ -0,0 +1,30 @@
+module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
+    console.log('')
+    console.log('== Profit deployment to', hre.network.name, '==')
+  
+    const { deploy, get } = deployments
+    const { deployer, devFund } = await getNamedAccounts()
+    const chainId = await getChainId()
+  
+    try {
+      const deplpoyment = await get('Profit')
+      console.log(
+        `Profit already deployed to ${hre.network.name} at ${deplpoyment.address}`
+      )
+      return
+    } catch (e) {
+      // not deployed yet
+    }
+  
+    console.log('ChainId:', chainId)
+    console.log('Deployer address:', deployer)
+  
+    await deploy('Profit', {
+      from: deployer,
+      log: true,
+    })
+    // need to set bridge contract as admin of synthetic profit
+  }
+  
+  module.exports.tags = ['Profit']
+  
